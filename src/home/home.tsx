2b53319dfe1f4f4306ad6e0619b3259ee6238371
Kenneth Elde Skjellvik <73293415+kenneth92skjellvik@users.noreply.github.com>
2023-05-06 07:04:40 +0200

    Update home.tsx

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -14,6 +14,8 @@ import EmailIcon from "@mui/icons-material/Email";
 import { Container, Row, Col} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CssBaseline } from "@mui/material";
+import { motion } from "framer-motion";
+
 interface User {
   _id: string;
   name: string;
@@ -60,6 +62,54 @@ const StyledCard = styled(Paper)(({ theme }) => ({
   marginBottom: theme.spacing(3),
 }));
 
+const bounceTransition = {
+  y: {
+    duration: 0.4,
+    yoyo: Infinity,
+    ease: "easeInOut",
+  },
+};
+
+const fadeIn = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const scaleUp = {
+  initial: { scale: 0 },
+  animate: {
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const staggeredChildren = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const slideRight = {
+  initial: { x: -100 },
+  animate: {
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 function Home({ user }: HomeProps) {
   const [loading, setLoading] = useState(true);
 
@@ -84,23 +134,33 @@ function Home({ user }: HomeProps) {
             <Container>
               <Row className="justify-content-center">
                 <Col xs={12} md={6} className="text-center">
-                  <LargeAvatar src="https://via.placeholder.com/150" />
-                  <WelcomeText variant="h4">
-                    Welcome, {user ? user.name : "Unknown"}!
-                  </WelcomeText>
-                  <LightTypography variant="subtitle1">
-                    Full Stack Developer
-                  </LightTypography>
+                  <motion.div initial="initial" animate="animate" variants={scaleUp}>
+                    <LargeAvatar src="https://via.placeholder.com/150" />
+                  </motion.div>
+                  <motion.div initial="initial" animate="animate" variants={fadeIn}>
+                    <WelcomeText variant="h4">
+                      Welcome, {user ? user.name : "Unknown"}!
+                    </WelcomeText>
+                    <LightTypography variant="subtitle1">
+                      Full Stack Developer
+                    </LightTypography>
+                  </motion.div>
                   <Box mt={2}>
-                    <IconButton>
-                      <GitHubIcon sx={{ color: "white" }} />
-                    </IconButton>
-                    <IconButton>
-                      <LinkedInIcon sx={{ color: "white" }} />
-                    </IconButton>
-                    <IconButton>
-                      <EmailIcon sx={{ color: "white" }} />
-                    </IconButton>
+                    <motion.div
+                      initial="initial"
+                      animate="animate"
+                      variants={slideRight}
+                    >
+                      <IconButton>
+                        <GitHubIcon sx={{ color: "white" }} />
+                      </IconButton>
+                      <IconButton>
+                        <LinkedInIcon sx={{ color: "white" }} />
+                      </IconButton>
+                      <IconButton>
+                        <EmailIcon sx={{ color: "white" }} />
+                      </IconButton>
+                    </motion.div>
                   </Box>
                 </Col>
               </Row>
@@ -109,54 +169,73 @@ function Home({ user }: HomeProps) {
 
           <Box mt={4}>
             <Container>
-              <LightTypography variant="h5">
-                Skills
-              </LightTypography>
-              <Row>
-                {[
-                  "React",
-                  "Node.js",
-                  "Python",
-                  "Django",
-                  "MongoDB",
-                  "Firebase",
-                ].map((skill, index) => (
-                  <Col key={index} xs={6} sm={4} md={2} className="mb-3">
-                    <StyledPaper elevation={1}>
-                      <Typography variant="subtitle1">{skill}</Typography>
-                    </StyledPaper>
-                  </Col>
-                ))}
-              </Row>
+              <motion.div initial="initial" animate="animate" variants={fadeIn}>
+                <LightTypography variant="h5">Skills</LightTypography>
+              </motion.div>
+              <motion.div initial="initial" animate="animate" variants={staggeredChildren}>
+                <Row>
+                  {[
+                    "React",
+                    "Node.js",
+                    "Python",
+                    "Django",
+                    "MongoDB",
+                    "Firebase",
+                  ].map((skill, index) => (
+                    <Col key={index} xs={6} sm={4} md={2} className="mb-3">
+                      <motion.div
+                        initial="initial"
+                        animate="animate"
+                        variants={slideRight}
+                      >
+                        <StyledPaper elevation={1}>
+                          <Typography variant="subtitle1">{skill}</Typography>
+                        </StyledPaper>
+                      </motion.div>
+                    </Col>
+                  ))}
+                </Row>
+              </motion.div>
             </Container>
           </Box>
 
           <Box mt={4}>
             <Container>
-              <LightTypography variant="h5">Portfolio</LightTypography>
-              <Row>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <Col key={index} xs={12} sm={6} md={4} className="mb-3">
-                    <StyledCard>
-                      <Typography variant="h6">Project Title</Typography>
-                      <Typography variant="body1">
-                        Project description
-                      </Typography>
-                    </StyledCard>
-                  </Col>
-                ))}
-              </Row>
+              <motion.div initial="initial" animate="animate" variants={fadeIn}>
+                <LightTypography variant="h5">Portfolio</LightTypography>
+              </motion.div>
+              <motion.div initial="initial" animate="animate" variants={staggeredChildren}>
+                <Row>
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <Col key={index} xs={12} sm={6} md={4} className="mb-3">
+                      <motion.div
+                        whileHover={{ scale: 1.1 }}
+                        transition={bounceTransition}
+                      >
+                        <StyledCard>
+                          <Typography variant="h6">Project Title</Typography>
+                          <Typography variant="body1">
+                            Project description
+                          </Typography>
+                        </StyledCard>
+                      </motion.div>
+                    </Col>
+                  ))}
+                </Row>
+              </motion.div>
             </Container>
           </Box>
 
           <Box mt={4} mb={4}>
             <Container>
-              <LightTypography variant="h5" >
-                Get in Touch
-              </LightTypography>
-              <Typography variant="body1">
-                Email: {user ? user.email : "example@example.com"}
-              </Typography>
+              <motion.div initial="initial" animate="animate" variants={fadeIn}>
+                <LightTypography variant="h5">Get in Touch</LightTypography>
+              </motion.div>
+              <motion.div initial="initial" animate="animate" variants={slideRight}>
+                <Typography variant="body1">
+                  Email: {user ? user.email : "example@example.com"}
+                </Typography>
+              </motion.div>
             </Container>
           </Box>
         </>
