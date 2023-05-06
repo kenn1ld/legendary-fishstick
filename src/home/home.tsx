3f6e79488ac3406f38078fc97bcc5f5d49da8873
Kenneth Elde Skjellvik <73293415+kenneth92skjellvik@users.noreply.github.com>
2023-05-06 06:44:36 +0200

    Update home.tsx

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+import {
+  Container,
+  Grid,
+  Paper,
+  Typography,
+  Box,
+  CircularProgress,
+  Avatar,
+  IconButton,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 interface User {
   _id: string;
@@ -11,7 +23,42 @@ interface User {
 interface HomeProps {
   user: User | null;
 }
-function Home({user}: HomeProps) {
+
+const AppContainer = styled(Container)(({ theme }) => ({
+  marginTop: theme.spacing(4),
+  marginBottom: theme.spacing(4),
+}));
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  minHeight: "200px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const WelcomeText = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(1),
+}));
+
+const SectionTitle = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+}));
+
+const CenteredBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const LargeAvatar = styled(Avatar)(({ theme }) => ({
+  width: theme.spacing(15),
+  height: theme.spacing(15),
+  marginBottom: theme.spacing(1),
+}));
+
+function Home({ user }: HomeProps) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -23,38 +70,70 @@ function Home({user}: HomeProps) {
   }, [user]);
 
   return (
-    <div className="App">
+    <AppContainer>
       {loading ? (
-        <h2>Loading...</h2>
-      ) : user ? (
-        <div>
-          <h2>Welcome, {user.name}!</h2>
-          <p>Email: {user.email}</p>
-          <p>Username: {user.username}</p>
-        </div>
+        <CenteredBox>
+          <CircularProgress />
+        </CenteredBox>
       ) : (
-        <h2>Please log in to view your information</h2>
-      )}
-      {/* Rest of the existing JSX */}
+        <>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={12} md={6}>
+              <LargeAvatar src="https://via.placeholder.com/150" />
+              <WelcomeText variant="h4">Welcome, {user ? user.name : "Unknown"}!</WelcomeText>
+              <Typography variant="subtitle1">Full Stack Developer</Typography>
+              <Box mt={2}>
+                <IconButton>
+                  <GitHubIcon />
+                </IconButton>
+                <IconButton>
+                  <LinkedInIcon />
+                </IconButton>
+                <IconButton>
+                  <EmailIcon />
+                </IconButton>
+              </Box>
+            </Grid>
+          </Grid>
 
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={1}>
-            {/* Add content here */}
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={1}>
-            
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Paper elevation={1}>
-            {/* Add content here */}
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
+          <Box mt={4}>
+            <SectionTitle variant="h5">Skills</SectionTitle>
+            <Grid container spacing={2}>
+              {["React", "Node.js", "Python", "Django", "MongoDB", "Firebase"].map(
+                (skill, index) => (
+                  <Grid item xs={6} sm={4} md={2} key={index}>
+                    <StyledPaper elevation={1}>
+                      <Typography variant="subtitle1">{skill}</Typography>
+                    </StyledPaper>
+                  </Grid>
+                )
+              )}
+            </Grid>
+          </Box>
+
+          <Box mt={4}>
+            <SectionTitle variant="h5">Portfolio</SectionTitle>
+            <Grid container spacing={2}>
+              {Array.from({ length: 6 }).map((_, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <StyledPaper elevation={1}>
+                    <Typography variant="h6">Project Title</Typography>
+                    <Typography variant="body1">Project description</Typography>
+                  </StyledPaper>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+
+          <Box mt={4} mb={4}>
+            <SectionTitle variant="h5">Get in Touch</SectionTitle>
+            <Typography variant="body1">
+              Email: {user ? user.email : "example@example.com"}
+            </Typography>
+          </Box>
+        </>
+      )}
+    </AppContainer>
   );
 }
 
